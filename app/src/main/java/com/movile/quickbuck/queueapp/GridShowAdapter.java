3809eb4cd2a9c1ad45e5c91565e3835ccc775319
ga23ma <gabriel_biel98@hotmail.com>
2015-10-31 16:26:12 -0200

    Menu

---
+++ app/src/main/java/com/movile/quickbuck/queueapp/GridShowAdapter.java
@@ -0,0 +1,62 @@
+package com.movile.quickbuck.queueapp;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+/**
+ * Created by android on 7/30/15.
+ */
+
+/**
+ * Created by android on 7/21/15.
+ */
+public class GridShowAdapter extends FragmentPagerAdapter {
+
+    public static final int POSITION_FIRST_CONTENT = 0;
+    public static final int POSITION_SECOND_CONTENT = 1;
+    public static final int POSITION_THIRD_CONTENT = 2;
+
+    private Context mContext;
+
+    public GridShowAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        mContext = context;
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == POSITION_FIRST_CONTENT) {
+            //return new PopularShowsFragment();
+        }
+        if (position == POSITION_SECOND_CONTENT) {
+            //return new PopularMoviesFragment();
+        }
+        if (position == POSITION_THIRD_CONTENT) {
+            //return new PopularMoviesFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == POSITION_FIRST_CONTENT) {
+            return "Place";
+        }
+        if (position == POSITION_SECOND_CONTENT) {
+            return "Join";
+        }
+        if (position == POSITION_THIRD_CONTENT) {
+            return"Status";
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/movile/quickbuck/queueapp/MenuActivity.java
+++ app/src/main/java/com/movile/quickbuck/queueapp/MenuActivity.java
@@ -1,7 +1,23 @@
 package com.movile.quickbuck.queueapp;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by hp on 31/10/2015.
  */
-public class MenuActivity {
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_activity);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.shows_view_pager);
+        pager.setAdapter(new GridShowAdapter(this.getSupportFragmentManager(), this));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.shows_tab);
+        tabLayout.setupWithViewPager(pager);
+    }
 }
