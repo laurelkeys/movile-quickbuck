21397fd2a74de2edb9d472dda4c38d08b6e671d2
FelipeCottaVivaldi <fcvgol@gmail.com>
2015-10-31 15:17:54 -0200

    Add firebase to login flow

---
+++ app/src/main/java/com/movile/quickbuck/queueapp/InitializeFirebase.java
@@ -0,0 +1,15 @@
+package com.movile.quickbuck.queueapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Felipe on 31/10/2015.
+ */
+public class InitializeFirebase extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- app/src/main/java/com/movile/quickbuck/queueapp/LoginActivity.java
+++ app/src/main/java/com/movile/quickbuck/queueapp/LoginActivity.java
@@ -1,5 +1,8 @@
 package com.movile.quickbuck.queueapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +11,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public final static String EXTRA_EMAIL = "EXTRA_EMAIL";
+    public final static String EXTRA_ID = "EXTRA_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,7 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
     }
 
     @Override
@@ -25,6 +38,35 @@ public class LoginActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
+    }
+
+    public void logarUsuario(View view) {
+        final EditText email = (EditText) findViewById(R.id.email_address);
+        EditText password = (EditText) findViewById(R.id.password);
+        if (email.getText().toString().equals("") || password.getText().toString().equals(""))
+            Toast.makeText(this, "Please fill all the fields correctly", Toast.LENGTH_LONG).show();
+        else
+        {
+            Firebase.setAndroidContext(this);
+            Firebase ref = new Firebase("https://amber-fire-875.firebaseio.com");
+            ref.authWithPassword(email.getText().toString(), password.getText().toString(), new Firebase.AuthResultHandler() {
+                @Override
+                public void onAuthenticated(AuthData authData) {
+                    Toast.makeText(getApplicationContext(), "Conectado com sucesso", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(this, MenuActivity.class);
+                    String emailUser = email.getText().toString();
+                    intent.putExtra(EXTRA_EMAIL, emailUser);
+                    String id = authData.getUid();
+                    intent.putExtra(EXTRA_ID, id);
+                }
+
+                @Override
+                public void onAuthenticationError(FirebaseError firebaseError) {
+                    Toast.makeText(getApplicationContext(), "Email or password wrong", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     }
 
     @Override
